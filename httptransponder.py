12e060d8b0e6684e289b574a719bafbf5079fc84
miguelsalva <miguelsalva@gmail.com>
2020-03-02 12:58:08 +0100

    Add function set_color() to httptransponder

--- httptransponder.py
+++ httptransponder.py
@@ -19,13 +19,29 @@ def get_status_code(ep):
     req = requests.get(ep)
     return req.status_code
 
+def set_color(sc):
+    """Function that gets an status code and returns a color"""
+    if sc in range(100,199):    # 1xx Informational response
+        return "\033[1;30;47m"
+    elif sc in range(200,299):  # 2xx Success
+        return "\033[1;37;42m"
+    elif sc in range(300,399):  # 3xx Redirection
+        return "\033[1;37;43m"
+    elif sc in range(400,499):  # 4xx Client errors
+        return "\033[1;37;41m"
+    elif sc in range(500,599):  # 5xx Server errors
+        return "\033[1;37;41m"
+    else:
+        return ""
+
 
 # Main
 OLD_STATUS_CODE = get_status_code(ENDPOINT)
 
 while True: 
     NEW_STATUS_CODE = get_status_code(ENDPOINT)
-    print("HTTP status code: " + str(NEW_STATUS_CODE) + " | Current HTTP downtime: " + str(DOWNTIME) + " seconds")
+    COLOR = set_color(NEW_STATUS_CODE)
+    print(str(COLOR) + "HTTP status code: " + str(NEW_STATUS_CODE) + " | Current HTTP downtime: " + str(DOWNTIME) + " seconds                               ")
     time.sleep(1)
     if OLD_STATUS_CODE != NEW_STATUS_CODE:
         OLD_STATUS_CODE = NEW_STATUS_CODE
@@ -34,7 +50,8 @@ while True:
 
 while True:
     NEW_STATUS_CODE = get_status_code(ENDPOINT)
-    print("HTTP status code: " + str(NEW_STATUS_CODE) + " | Current HTTP downtime: " + str(DOWNTIME) + " seconds")
+    COLOR = set_color(NEW_STATUS_CODE)
+    print(str(COLOR) + "HTTP status code: " + str(NEW_STATUS_CODE) + " | Current HTTP downtime: " + str(DOWNTIME) + " seconds                               ")
     time.sleep(1)
     if OLD_STATUS_CODE != NEW_STATUS_CODE:
         break  # Second HTTP status code change
